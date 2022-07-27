68153a760eb94104332d0fac5b6640752f295c03
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2022-07-27 18:10:06 -0400

    v1.1.14

--- src/main/java/com/gggame/randomthings/event/ClientEvent.java
+++ src/main/java/com/gggame/randomthings/event/ClientEvent.java
@@ -1,14 +1,11 @@
 package com.gggame.randomthings.event;
 
 import com.gggame.randomthings.Main;
-import com.gggame.randomthings.init.BlockInit;
 import com.gggame.randomthings.init.WoodTypeInit;
 import com.gggame.randomthings.screen.MapleFurnaceScreen;
 import com.gggame.randomthings.screen.MenuTypes;
 import com.gggame.randomthings.screen.OreExtractorScreen;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,14 +16,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ClientEvent {
     @SubscribeEvent
     public static void clientSetup(final FMLClientSetupEvent event) {
-        ItemBlockRenderTypes.setRenderLayer(BlockInit.ORE_EXTRACTOR.get(), RenderType.cutout());
-
-        ItemBlockRenderTypes.setRenderLayer(BlockInit.CHERRY_LEAVES.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(BlockInit.MAPLE_LEAVES.get(), RenderType.cutout());
-
-        ItemBlockRenderTypes.setRenderLayer(BlockInit.CHERRY_SAPLING.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(BlockInit.MAPLE_SAPLING.get(), RenderType.cutout());
-
         MenuScreens.register(MenuTypes.ORE_EXTRACTOR_MENU.get(), OreExtractorScreen::new);
         MenuScreens.register(MenuTypes.MAPLE_FURNACE_MENU.get(), MapleFurnaceScreen::new);
 
